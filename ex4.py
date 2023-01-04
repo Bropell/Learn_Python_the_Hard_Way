ddd83c88602f1daafc7f193277ae1fc0b4c08651
Brandon Ropell <brandon.ropell@gmail.com>
2023-01-04 13:06:51 -0500

    Updated print format for exercise 4

--- ex4.py
+++ ex4.py
@@ -16,10 +16,10 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-# Used f-string method to print the strings with the values inside the curly braces
-print(f"There are {cars} cars available.")
-print(f"There are only {drivers} drivers available.")
-print(f"There will be {cars_not_driven} empty cars today.")
-print(f"We can transport {carpool_capacity} people today.")
-print(f"We have {passengers} to carpool today.")
-print(f"We need to put about {average_passengers_per_car} in each car")
+# Print the strings with the variables in between
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car")
