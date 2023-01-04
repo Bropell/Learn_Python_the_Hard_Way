d7acc8c7ad5cb0bc4ef8c24d76649dcc66681acd
Brandon Ropell <brandon.ropell@gmail.com>
2023-01-04 12:09:06 -0500

    Added exercise 4

---
+++ ex4.py
@@ -0,0 +1,25 @@
+# Assigned the cars variable an integer value of 100
+cars = 100
+# Assigned the space_in_a_car variable a floating point value of 4.0
+space_in_a_car = 4.0
+# Assigned the drivers variable an integer value of 30
+drivers = 30
+# Assigned the passengers variable an integer value of 90
+passengers = 90
+# Assigned the cars_not_driven variable equal to the cars variable minus the drivers variable
+cars_not_driven = cars - drivers
+# Assigned the cars_driven variable equal to the drivers variable
+cars_driven = drivers
+# Assigned the carpool_capacity variable equal to the cars_driven variable multiplied by the space_in_a_car variable
+carpool_capacity = cars_driven * space_in_a_car
+# Assigned the average_passengers_per_car variable equal to the passengers variable divided by the cars_driven variable
+average_passengers_per_car = passengers / cars_driven
+
+
+# Used f-string method to print the strings with the values inside the curly braces
+print(f"There are {cars} cars available.")
+print(f"There are only {drivers} drivers available.")
+print(f"There will be {cars_not_driven} empty cars today.")
+print(f"We can transport {carpool_capacity} people today.")
+print(f"We have {passengers} to carpool today.")
+print(f"We need to put about {average_passengers_per_car} in each car")
