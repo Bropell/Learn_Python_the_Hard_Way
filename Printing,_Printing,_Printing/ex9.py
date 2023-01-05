86c4dd15b6f44f72394edc256ca6413ca99ec5aa
Brandon Ropell <brandon.ropell@gmail.com>
2023-01-05 12:53:23 -0500

    Added Printing, Printing, Printing folder with exercise 9

---
+++ Printing,_Printing,_Printing/ex9.py
@@ -0,0 +1,17 @@
+# Assign the variable days to the string "Mon Tue Wed Thu Fri Sat Sun"
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assign the variable months to the string "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug". The \n puts what follows on a new line.
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Prints "Here are the days: Mon Tue Wed Thu Fri Sat Sun" 
+print("Here are the days: ", days)
+# Prints "Here are the months: Jan Feb Mar Apr May Jun Jul Aug" with each month on its own line
+print("Here are the months: ", months)
+
+# Prints the long string below. Triple quotes allows multi-line strings
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
