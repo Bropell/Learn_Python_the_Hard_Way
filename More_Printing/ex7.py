b2f7a97c2cea967f28c030d8fc991a731caa4ec8
Brandon Ropell <brandon.ropell@gmail.com>
2023-01-05 12:15:46 -0500

    Added More Printing folder with exercise 7

---
+++ More_Printing/ex7.py
@@ -0,0 +1,26 @@
+# Prints the string "Mary had a little lamb."
+print("Mary had a little lamb.")
+# Prints the string "Its fleese was white as snow" using the .format syntax
+print("Its fleese was white as {}.".format('snow'))
+# Prints the string "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+# Prints the string "." ten times on one line
+print("." * 10) 
+
+# Assign the variables on the left to the string values on the right of the equals sign
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Prints the summation of the variables on the same line with a space bewteen the two prints 
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
