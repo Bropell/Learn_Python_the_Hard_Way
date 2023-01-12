bc083fa724f885879b0058c962f311f05acd262f
Brandon Ropell <brandon.ropell@gmail.com>
2023-01-11 21:49:40 -0500

    Added Prompting People folder with exercise 12

---
+++ Prompting_People/ex12.py
@@ -0,0 +1,6 @@
+# Setting variables equal to user input more concisely than exercise 11
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
